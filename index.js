e2512c9f313cfb1868aead667b9d462f433ce0a1
Nikapwente <hrsmajor@gmail.com>
2023-10-11 11:33:22 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (drivers, driverName) {
+    let selectedDrivers = drivers.filter(function(drivers) {return drivers === driverName || drivers == driverName.toLowerCase();});
+    return selectedDrivers;
+}
+
+function fuzzyMatch (drivers, driverName) {
+    let selectedDrivers = drivers.filter(function(drivers) {return drivers.startsWith(driverName);});
+    return selectedDrivers;
+}
+
+function matchName (drivers, property) {
+    let selectedDrivers = drivers.filter(function(drivers) {return drivers.name === property;})
+    return selectedDrivers;
+}
